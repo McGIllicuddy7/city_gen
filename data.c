6270001554b627e9b0edd7d6960ac7625a987b71
Isaac <IsaacPasco@icloud.com>
2023-10-27 23:34:05 -0700

    i 	new file:   data.c 	new file:   data.h 	modified:   city_generator 	modified:   generator.c 	modified:   generator.h

---
+++ data.c
@@ -0,0 +1,12 @@
+#include "data.h"
+#include "generator.h"
+static int icount = 0;
+typedef enum{
+    House,Cobbler, Tailor,Barber,Jewelers, Tavern, Bakers,Butchers, Masons,Weavers, WineSellers, Hatmakers,Saddlers, Woodsellers,MagicShop, BookBinders, FishMongers, BeerSellers,BuckleMakers, Plasterers, SpiceMerchants,Blacksmiths,Painters,Doctors,Roofers,Locksmiths,Ropemakers,Inn,Copyists,Tanners,Glovemakers,Booksellers
+}building_e;
+building_e calculate_next(building_t * buildings){
+    
+}
+void calculate_building_data(building_t * buildings, int * interesting){
+    int numbuildings = getNumBuildings();
+}
---
+++ data.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "generator.h"
+void calculate_building_data(building_t * buildings, int * interesting_indices);
+    
